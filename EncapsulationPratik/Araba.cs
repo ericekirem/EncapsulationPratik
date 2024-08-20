83dd1501e0cfc8a37ffd53eac91ecb0681a9cfa4
ericekirem <irem.ericek@icloud.com>
2024-08-20 11:23:42 +0300

    Add project files.

---
+++ EncapsulationPratik/Araba.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EncapsulationPratik
+{
+
+    public class Araba
+    {
+        // Property'ler
+        public string Marka { get; set; }
+        public string Model { get; set; }
+        public string Renk { get; set; }
+
+        private int kapiSayisi;
+
+        // Kapı Sayısı için encapsulation (kapsülleme)
+        public int KapiSayisi
+        {
+            get { return kapiSayisi; }
+            set
+            {
+                if (value == 2 || value == 4)
+                {
+                    kapiSayisi = value;
+                }
+                else
+                {
+                    Console.WriteLine("Uyarı: Kapı sayısı sadece 2 veya 4 olabilir! Kapı sayısı -1 olarak ayarlandı.");
+                    kapiSayisi = -1;
+                }
+            }
+        }
+
+        // Constructor
+        public Araba(string marka, string model, string renk, int kapiSayisi)
+        {
+            Marka = marka;
+            Model = model;
+            Renk = renk;
+            KapiSayisi = kapiSayisi; // Bu, KapiSayisi property'nin set metodunu çağırır.
+        }
+
+        // Araba bilgilerini yazdırmak için bir metot
+        public void PrintInfo()
+        {
+            Console.WriteLine($"Marka: {Marka}, Model: {Model}, Renk: {Renk}, Kapı Sayısı: {KapiSayisi}");
+        }
+    }
+
+}
---
+++ EncapsulationPratik/Program.cs
@@ -0,0 +1,21 @@
+﻿using EncapsulationPratik;
+using System;
+
+namespace ArabaApp
+{
+   
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Geçerli kapı sayısı (2) ile araba nesnesi oluşturma
+            
+            Araba araba1 = new Araba("Mercedes", "GLE", "Gri", 2);
+            araba1.PrintInfo();
+
+            // Geçersiz kapı sayısı (3) ile araba nesnesi oluşturma
+            Araba araba2 = new Araba("Porsche", "Taycan", "Siyah", 3);
+            araba2.PrintInfo();
+        }
+    }
+}
